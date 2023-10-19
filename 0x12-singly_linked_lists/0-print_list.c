fa6c5b9d3999810ef7eeaa6deecb466dfb56207c
Rahmaihab <rahmaihab6@gmail.com>
2023-10-19 12:14:25 -0700

    gh

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 /**
-* print_list - prints all the elements of a linked list
-* @h: pointer to the list_t list to print
+* free_list - Realease the memory allocated for a list
 *
-* Return: the number of nodes printed
+* @head: A pointer to the first node of the list to free
 */
-size_t print_list(const list_t *h)
+void free_list(list_t *head)
 {
-size_t s = 0;
-while (h)
+if (head)
 {
-if (!h->str)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
-h = h->next;
-s++;
-}
-return (s);
+free_list(head->next);
+if (head->str)
+free(head->str);
+free(head);
 }
